c6e5f11b4701e5b48e5ae95cdd5674c1702cdd3a
avanyushkin <a.vanyushkin1@gmail.com>
2026-03-20 16:05:44 +0300

    feat (feature/task-04): add handle click logic

--- src/tasks/task-04/Solve.tsx
+++ src/tasks/task-04/Solve.tsx
@@ -12,10 +12,19 @@ import "./styles.css";
 const TOTAL_AMOUNT: number = 10;
 const DUBLICATE: number = 2;
 const MATRIX: number = 9;
+const SHOW_TIME: number = 3000;
 
 function Solve () {
   const [imageIndexes, setImageIndexes] = useState<number[]> ([]);
-  useEffect (() => {
+  const [wins, setWins] = useState<number> (0);
+  const [games, setGames] = useState<number> (0);
+  const [isVisible, setIsVisible] = useState<boolean> (true);
+
+  const [selectedIndexes, setSelectedIndexes] = useState<number[]> ([]);
+  const [selectedPositions, setSelectedPositions] = useState<number[]> ([]);
+  const [isProcessing, setIsProcessing] = useState<boolean> (false);
+  
+  const initializeGame = () => {
     let initArray: Array<number> = [];
     for (let i: number = 1; i <= TOTAL_AMOUNT; i += 1) {
       initArray.push (i);
@@ -34,18 +43,64 @@ function Solve () {
     // console.log ("initArray: ", initArray);
     initArray.sort (() => Math.random () - 0.5);
     setImageIndexes (initArray);
-  }, []);
+
+    setSelectedIndexes ([]);
+    setSelectedPositions ([]);
+    setIsProcessing (false);
+
+    setIsVisible (true);
+
+    const timer = setTimeout (() => {
+      setIsVisible (false);
+    }, SHOW_TIME);
+
+    return () => clearTimeout (timer);
+  }
+
+  useEffect (() => {
+    initializeGame ();
+  }, [games]);
   // console.log(imageIndexes);
+
+  const handleImageClick = (position: number, imageIndex: number) => {
+    if (isVisible || isProcessing || selectedPositions.includes (position)) {
+      return;
+    }
+    const newSelectedIndexes = [...selectedIndexes, imageIndex];
+    const newSelectedPositions = [...selectedPositions, position];
+    setSelectedIndexes (newSelectedIndexes);
+    setSelectedPositions (newSelectedPositions);
+
+    if (newSelectedIndexes.length === 2) {
+      setIsProcessing (true);
+      if (newSelectedIndexes[0] === newSelectedIndexes[1]) {
+        setWins (prev => prev + 1);
+      } else {
+        setGames (prev => prev + 1);
+        return;
+      }
+
+      setGames (prev => prev + 1);
+      setTimeout (() => {
+        setSelectedIndexes ([]);
+        setSelectedPositions ([]);
+        setIsProcessing (false);
+      }, 500);
+    }
+  }
+
   return (
   <>
     <div className="images-board">
       {
-        imageIndexes.map(index => {
+        imageIndexes.map( (index, position) => {
           try {
             const imageUrl = new URL(`./images/image${index}.jpg`, import.meta.url).href;
             return (
-              <div className="image" key={Date.now ()}>
-                <img src={imageUrl} alt={`image${index}`}/>
+              <div className = {`image ${isVisible ? 'visible' : 'hidden'} ${selectedPositions.includes(position) ? 'selected' : ''}`} 
+                   key = {`${index} - ${position}`}
+                   onClick = {() => handleImageClick (position, index)}>
+                <img src = {imageUrl} alt = {`image${index}`}/>
               </div>
             );
           } catch (error) {
@@ -54,6 +109,10 @@ function Solve () {
           }
         })
       }
+    </div>
+    <div className = "score-board">
+      <div className = "wins-amount">Wins - {wins}</div>
+      <div className = "loses-amount">Loses - {games - wins}</div>
     </div>
   </>
 );
