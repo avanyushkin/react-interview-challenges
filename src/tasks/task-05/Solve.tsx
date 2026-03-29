1794304790508517c4fcfc64ce4df8784e26a4b4
avanyushkin <a.vanyushkin1@gmail.com>
2026-03-29 17:36:08 +0300

    feat (feature/task-05): add styles

--- src/tasks/task-05/Solve.tsx
+++ src/tasks/task-05/Solve.tsx
@@ -32,11 +32,27 @@ type TEntry = {
 };
 
 function Entry ({ entry, depth }: { entry: TEntry; depth: number } ) {
+  const [isExpanded, setIsExpanded] = useState<boolean> (false);
   return (
-    <div style = {{paddingLeft: `${depth * 10}px`}}>
-      {entry.name}
-      {entry.children?.map ((entry) => (<Entry entry = {entry} depth = {depth + 1} />))}
-    </div>
+    <>
+      {entry.children ? (
+      <button onClick = {() => setIsExpanded (!isExpanded)} className = "button-name">
+        {entry.children && "+ "}
+        {entry.name}
+      </button>
+
+      ) : (
+        <div>
+          {entry.children && "+ "}
+          {entry.name}
+        </div>
+      )}
+      {isExpanded && (
+      <div style = {{paddingLeft: `${depth * 10}px`}}>
+        {entry.children?.map ((entry) => (<Entry entry = {entry} depth = {depth + 1} />))}
+      </div>
+      )}
+    </>
   );
 }
 
